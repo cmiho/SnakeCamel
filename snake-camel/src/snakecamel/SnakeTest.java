24dd886cd1dd66ec9321375af2f12dcc1b24d9e3
g14902cm <g14902cm@s30433.tsuda.ac.jp>
2016-05-06 12:01:26 +0900

    てすとの中身書いてみた

--- snake-camel/src/snakecamel/SnakeTest.java
+++ snake-camel/src/snakecamel/SnakeTest.java
@@ -8,7 +8,30 @@ public class SnakeTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		fail("できてないよ");
+		
+	}
+	@Test
+	public void snakeToCamelcaseでキャメルケースを得る() {
+		//SnakeCamelUtil snake = new SnakeCamelUtil();
+		String expected = "AbcDefGhi";
+		String actual = SnakeCamelUtil.snakeToCamelcase("abc_def_ghi");
+		assertEquals(actual,expected);
+		
+	}
+	@Test
+	public void camelToSnakecaseでスネークケースを得る() {
+		//SnakeCamelUtil snake = new SnakeCamelUtil();
+		String expected = "abc_def_ghi";
+		String actual = SnakeCamelUtil.camelToSnakecase("AbcDefGhi");
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void capitalizeで頭文字大文字を得る() {
+		//SnakeCamelUtil snake = new SnakeCamelUtil();
+		String expected = "abc";
+		String actual = SnakeCamelUtil.capitalize("Abc");
+		assertEquals(actual,expected);
 	}
 
 }
